141367850052ee2f8b18d8f92bda340ed9bec75f
Camryn Poole <camrynannpoole@gmail.com>
2022-10-15 13:17:26 -0400

    feat: show octoface if copilot is active

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -68,16 +68,36 @@ end
 
 -- display lsp name, if no lsp then display nothing
 local lsp_server_name = function()
-	local msg = " "
+	local msg = ""
+	local buf_clients = vim.lsp.get_active_clients()
 	local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-	local clients = vim.lsp.get_active_clients()
-	if next(clients) == nil then
+
+	if next(buf_clients) == nil then
 		return msg
 	end
-	for _, client in ipairs(clients) do
+
+	for _, client in ipairs(buf_clients) do
 		local filetypes = client.config.filetypes
 		if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
 			return " " .. client.name
+		end
+	end
+	return msg
+end
+
+local copilot = function()
+	local msg = ""
+	local buf_clients = vim.lsp.get_active_clients()
+	local buf_client_names = {}
+
+	if next(buf_clients) == nil then
+		return msg
+	end
+
+	for _, client in pairs(buf_clients) do
+		table.insert(buf_client_names, client.name)
+		if client.name == "copilot" then
+			return ""
 		end
 	end
 	return msg
@@ -102,7 +122,7 @@ lualine.setup({
 			{ spaces },
 			{ lsp_server_name, icons_enabled = true },
 		},
-		lualine_y = { filetype },
+		lualine_y = { copilot, filetype },
 		lualine_z = {},
 	},
 })
